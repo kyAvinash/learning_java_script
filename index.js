b50bfc96d1d122225b6f419ed8f8396bd12ac2f3
avinash <ky0avinash@gmail.com>
2023-08-07 05:21:18 -0400

    variables by Mosh Tutorial

---
+++ index.js
@@ -0,0 +1,39 @@
+// This is my first and complete java script tutorial.
+console.log('Hello World!')
+
+
+let name="Avinsh";
+console.log(name);
+
+// rules of Variables- they cannot be reserved keywords.
+// They should be meaningful.
+// They cannot start with number (1name)
+// They cnnot be a space or hyphen(-)
+// They are case sensitive.
+
+let firstName;  // this is camel notation. Its recommended to use it.
+let FirstName;
+
+// There are two ways to declear the variable in JS.
+
+let firstName1="Aviansh", lastName="yadav";
+
+
+// That is the modern way and best practice now a days.
+let firstName2="Avinash";
+let lastName2="Yadav";
+
+console.log("name= "+name);
+console.log("first name= "+firstName1);
+console.log("last name= "+lastName);
+console.log("first name2= "+firstName2);
+console.log("last name2= "+lastName2);
+
+
+
+/* 
+Now we will learn about constants in this only.
+*/ 
+
+
+
